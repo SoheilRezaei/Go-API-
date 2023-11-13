1c32a7bce0a378242c46970d29582ac380907f6f
SoheilRezaei <113804552+SoheilRezaei@users.noreply.github.com>
2023-11-13 06:34:31 +0000

    indentation fix

--- internal/handlers/get_coin_balance.go
+++ internal/handlers/get_coin_balance.go
@@ -16,38 +16,39 @@ func GetCoinBalance(w http.ResponseWriter, r *http.Request) {
 	var err error
 
 	err = decoder.Decode(&params, r.URL.Query())
-}
 
-if err != nil {
-	log.Error(err)
-	api.InternalErrorHandler(w)
-	return
-}
 
-var database *tools.DatabaseInterface
-database, err = tools.NewDatabase()
-if err != nil {
-	api.InternalErrorHandler(w)
-	return
-}
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return
+	}
 
-var tokenDetails *tools.CoinDetails
-tokenDetails = (*database).GetUserCoins(params.Username)
-if tokenDetails == nil {
-	log.Error(err)
-	api.InternalErrorHandler(w)
-	return
-}
+	var database *tools.DatabaseInterface
+	database, err = tools.NewDatabase()
+	if err != nil {
+		api.InternalErrorHandler(w)
+		return
+	}
 
-var response = api.CoinBalanceResponse{
-	Balance: (*tokenDetails).Coins,
-	Code: http.StatusOK,
-}
+	var tokenDetails *tools.CoinDetails
+	tokenDetails = (*database).GetUserCoins(params.Username)
+	if tokenDetails == nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return
+	}
 
-w.header().Set("Content-Type","application/json")
-err = json.NewEncoder(w).Encode(response)
-if err != nil {
-	log.Error(err)
-	api.InternalErrorHandler(w)
-	return
+	var response = api.CoinBalanceResponse{
+		Balance: (*tokenDetails).Coins,
+		Code: http.StatusOK,
+	}
+
+	w.header().Set("Content-Type","application/json")
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return
+	}
 }
